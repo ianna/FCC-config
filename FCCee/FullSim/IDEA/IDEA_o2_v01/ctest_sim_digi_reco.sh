64da0c5ad315448ecc2e1dd2e0afa0741fd6ef2c
Ianna Osborne <ianna.osborne@cern.ch>
2025-03-07 16:02:15 +0100

    add

---
+++ FCCee/FullSim/IDEA/IDEA_o2_v01/ctest_sim_digi_reco.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# set-up the Key4hep environment if not already set
+if [[ -z "${KEY4HEP_STACK}" ]]; then
+  source /cvmfs/sw-nightlies.hsf.org/key4hep/setup.sh
+else
+  echo "The Key4hep stack was already loaded in this environment."
+fi
+
+# workaround to have ctests working
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# run the SIM step
+ddsim --enableGun --gun.distribution uniform --gun.energy "10*GeV" --gun.particle e- --numberOfEvents 10 --outputFile IDEA_sim.root --random.enableEventSeed --random.seed 42 --compactFile $K4GEO/FCCee/IDEA/compact/IDEA_o2_v01/IDEA_o2_v01.xml --steeringFile $SCRIPT_DIR/SteeringFile_IDEA_o2_v01.py
+
+# run the DIGI/RECO step
+k4run $SCRIPT_DIR/run_digi_reco.py
